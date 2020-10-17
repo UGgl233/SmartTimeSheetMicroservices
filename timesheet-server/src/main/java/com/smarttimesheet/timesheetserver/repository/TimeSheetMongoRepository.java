e068f54b710774a1c58b59b7b42996d836d139e5
holidayliu <liuxiaoyu.holiday@gmail.com>
2020-10-17 16:25:56 -0400

    data displayed

--- timesheet-server/src/main/java/com/smarttimesheet/timesheetserver/repository/TimeSheetMongoRepository.java
+++ timesheet-server/src/main/java/com/smarttimesheet/timesheetserver/repository/TimeSheetMongoRepository.java
@@ -1,5 +1,6 @@
 package com.smarttimesheet.timesheetserver.repository;
 
+import com.smarttimesheet.timesheetserver.domain.Details;
 import com.smarttimesheet.timesheetserver.domain.Timesheet;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
@@ -7,5 +8,6 @@ import java.util.List;
 
 public interface TimeSheetMongoRepository extends MongoRepository<Timesheet, String>{
     List<Timesheet> findByName(String name);
+    List<Details> findDetailsByName(String name);
 
 }
--- timesheet-server/src/main/java/com/smarttimesheet/timesheetserver/service/TimesheetService.java
+++ timesheet-server/src/main/java/com/smarttimesheet/timesheetserver/service/TimesheetService.java
@@ -5,7 +5,6 @@ import com.smarttimesheet.timesheetserver.domain.Timesheet;
 import com.smarttimesheet.timesheetserver.repository.TimeSheetMongoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.Arrays;
 import java.util.List;
 
@@ -25,19 +24,27 @@ public class TimesheetService {
         Details day6 = Details.builder().day("Saturday").startingTime(-1).endingTime(-1).totalHours(0).build();
         Details day7 = Details.builder().day("Sunday").startingTime(-1).endingTime(-1).totalHours(0).build();
 
-//        Details Phillip = null;
-//        Phillip.builder().date("Monday").startingTime(9).endingTime(18).build();
-//
-//        Details Grace = null;
-//        Grace.builder().date("Tuesday").startingTime(9).endingTime(18).build();
+        repository.deleteAll();
 
+        repository.save(Timesheet.builder().name("1").totalHours(45).submissionStatus("Complete").
+                approvalStatus("approved").weekEnding("3/17/2018").comment("1 Floating Day Required")
+                .details(Arrays.asList(day1, day2, day3, day4, day5, day6, day7)).build());
+        repository.save(Timesheet.builder().name("2").totalHours(40).submissionStatus("Not Complete").
+                approvalStatus("N/A").weekEnding("3/10/2018").comment("1 Floating Day Required")
+                .details(Arrays.asList(day1, day2, day3, day4, day5, day6, day7)).build());
+        repository.save(Timesheet.builder().name("2").totalHours(35).submissionStatus("Not Complete").
+                approvalStatus("N/A").weekEnding("3/03/2018").comment("1 Floating Day Required")
+                .details(Arrays.asList(day1, day2, day3, day4, day5, day6, day7)).build());
+        repository.save(Timesheet.builder().name("2").totalHours(36).submissionStatus("Not Complete").
+                approvalStatus("N/A").weekEnding("2/25/2018").comment("1 Floating Day Required")
+                .details(Arrays.asList(day1, day2, day3, day4, day5, day6, day7)).build());
+        repository.save(Timesheet.builder().name("2").totalHours(40).submissionStatus("Not Complete").
+                approvalStatus("N/A").weekEnding("2/18/2018").comment("1 Floating Day Required")
+                .details(Arrays.asList(day1, day2, day3, day4, day5, day6, day7)).build());
+        repository.save(Timesheet.builder().name("2").totalHours(40).submissionStatus("Not Complete").
+                approvalStatus("N/A").weekEnding("2/11/2018").comment("1 Floating Day Required")
+                .details(Arrays.asList(day1, day2, day3, day4, day5, day6, day7)).build());
 
-        repository.save(Timesheet.builder().name("Phillip").submissionStatus("Complete").
-                approvalStatus("N/A").weekEnding("3/10/2018")
-                .comment("1 Floating Day Required").details(Arrays.asList(day1)).build());
-        repository.save(Timesheet.builder().name("Grace").submissionStatus("Not Complete").
-                approvalStatus("N/A").weekEnding("3/17/2018")
-                .comment("1 Floating Day Required").build());
     }
 
     //return all document
@@ -45,4 +52,12 @@ public class TimesheetService {
 
     //return document by name
     public List<Timesheet> findByName(String name) {return repository.findByName(name);}
+
+    //find details by name
+//    public List<Details> findDetailsByName(String name) {
+//        List<Details> list = new ArrayList<>();
+//        repository.findByName("1").indexOf(Details.class);
+//
+//        return null;
+//    }
 }
